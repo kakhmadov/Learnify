71c9ef521de237ac5256b724897ff84080aa8d78
kakhmadov <kakhmadov@student.tgm.ac.at>
2025-05-28 13:51:00 +0200

    Added more backend files

---
+++ code/backend/config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'db' => [
+        'host'     => 'localhost',
+        'dbname'   => 'db3dHIT_s1',
+        'user'     => 'db3dHIT_s1',
+        'password' => 'ohpiTh7u',
+        'charset'  => 'utf8mb4',
+    ],
+];
